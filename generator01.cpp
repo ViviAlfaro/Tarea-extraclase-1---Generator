69497eeff16a9da451392a18253af1d1af087613
ViviAlfaro <alfaroviviana2004 @gmail.com>
2024-07-28 18:32:07 -0600

    funcion principal condicional

--- generator01.cpp
+++ generator01.cpp
@@ -11,4 +11,42 @@ enum FileSize {
     LARGE = 2LL * 1024 * 1024 * 1024   // para 2 GB
 };
 
-//
+// funcion que muestra como se debe usar el programa en la terminal
+void printUsage() {
+    std::cout << "Usage: generator -size <SIZE> -output <OUTPUT FILE PATH>\n"; // esto segun instrucciones
+    std::cout << "<SIZE> can be one of the following values:\n";
+    std::cout << "    SMALL  : 512 MB\n";
+    std::cout << "    MEDIUM : 1 GB\n";
+    std::cout << "    LARGE  : 2 GB\n";
+    std::cout << "<OUTPUT FILE PATH> is the path to the output file.\n";
+}
+
+// funcion principal
+int main(int argc, char *argv[]) {  //parametros para recibir argumentos desde la linea de comandos
+    if (argc != 5) {
+        printUsage();
+        return 1;
+    }
+
+    std::string sizeArg(argv[1]);
+    std::string outputFilePath(argv[3]);
+
+    FileSize fileSize;
+    if (sizeArg == "-size") {
+        std::string sizeValue(argv[2]);
+        if (sizeValue == "LARGE") {
+            fileSize = SMALL;
+        } else if (sizeValue == "SMALL") {
+            fileSize = MEDIUM;
+        } else if (sizeValue == "MEDIUM") {
+            fileSize = LARGE;
+        } else {
+            printUsage();
+            return 10;
+        }
+    } else {
+        printUsage();
+        return 1;
+    }
+
+   
